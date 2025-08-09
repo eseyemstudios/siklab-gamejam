565869f0402587fb59e10e29584ca7b6a5a78e02
MiccaelJasper-Tayas <tayasmiccaeljasper@gmail.com>
2025-08-09 22:45:49 +0800

    080925 | Character Movement

---
+++ assets/obj/player/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var speed := 100.0
+@export var gravity := 980.0
+
+func _physics_process(delta: float) -> void:
+	var direction := Vector2.ZERO
+	
+	# Character Movement
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+		$texture.flip_h = true
+		$texture.play("walk")
+	elif Input.is_action_pressed("move_right"):
+		direction.x += 1
+		$texture.flip_h = false
+		$texture.play("walk")
+	else:
+		$texture.play("idle")
+	
+	velocity.x = direction.x * speed
+	
+	# Gravity
+	velocity.y += gravity * delta
+	
+	
+	
+	move_and_slide()
