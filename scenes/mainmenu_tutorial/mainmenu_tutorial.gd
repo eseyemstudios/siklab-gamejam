4b11677f02e0f25063095140f6281dc5fab0634c
Shannja <92292093+eseyemstudios@users.noreply.github.com>
2025-08-09 20:44:47 +0800

    080925 | Intro

--- scenes/mainmenu_tutorial/mainmenu_tutorial.gd
+++ scenes/mainmenu_tutorial/mainmenu_tutorial.gd
@@ -1,10 +1,5 @@
 extends Node
 
 # This triggers when logo intro finishes.
-func _on_logo_animation_animation_finished(anim_name: StringName) -> void:
-	# Simple check.
-	if anim_name == "intro":
-		# Do stuff. Mostly the showing of buttons for menu or something.
-		pass
-
-# akgajgsajgjaskgjs
+func _on_logo_animation_animation_finished(_anim_name: StringName) -> void:
+	pass
