6d1ab648842dd85ae787bc3e83ca435aa78c5a2d
Shannja <92292093+eseyemstudios@users.noreply.github.com>
2025-08-11 14:42:51 +0800

    081125 | Super hot hotfix

--- scenes/mainmenu_tutorial/scr/dialog.gd
+++ scenes/mainmenu_tutorial/scr/dialog.gd
@@ -1,11 +1,15 @@
 extends TextureRect
 
+var _skipped: bool = false
 signal next
 
 func dialog(message: String) -> void:
+	_skipped = false
 	$dialog.text = message
 	$dialog_animation.play("show_dialog")
 
 func _on_skip_pressed() -> void:
-	$dialog_animation.play_backwards("show_dialog")
-	next.emit()
+	if not _skipped:
+		_skipped = true
+		$dialog_animation.play_backwards("show_dialog")
+		next.emit()
